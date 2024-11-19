96997aae18f2265ea1701694d019aded50474e16
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-11-19 13:56:48 -0600

    chore: Dropped support for Python 3.8 and use Ruff to lint (#98)

--- cz_version_bump/git.py
+++ cz_version_bump/git.py
@@ -2,10 +2,11 @@ import subprocess
 
 
 def repo_name_from_git_remote() -> str:
-    git_remote_output = subprocess.run(
+    git_remote_output = subprocess.run(  # noqa: S603
         ("git", "remote", "-v"),
         stdout=subprocess.PIPE,
         text=True,
+        check=False,
     ).stdout
     github_remote = next(x for x in git_remote_output.split() if "github.com" in x)
 
